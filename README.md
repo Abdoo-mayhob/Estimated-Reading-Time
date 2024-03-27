0470a0ebdbaac6e4369919a5f1d75c3de822038e
a.almayhob <a.almayhob@haykalmedia.com>
2024-03-27 18:53:53 +0300

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# Estimated-Reading-Time
-Wordpress Plugin to Calculate Estimated Reading Time for a blog post in Both Arabic and English.
+# Estimated Reading Time
+
+**Contributors:** abdoomayhob  
+**Tags:** app, store, redirect, ios, android  
+**Requires at least:** 5.0  
+**Tested up to:** 6.4.3  
+**Requires PHP:** 7.0  
+**Stable tag:** 1.2  
+**License:** GPLv2 or later  
+**License URI:** [https://www.gnu.org/licenses/gpl-3.0.html](https://www.gnu.org/licenses/gpl-3.0.html)  
+
+## Description
+
+Wordpress Plugin to Calculate and Show Estimated Reading Time in your posts in Both Arabic and English.
+
+## Installation
+
+1. Upload the `est-read-time` folder to the `/wp-content/plugins/` directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. Configure the plugin by navigating to 'Estimated Reading Time' in the WordPress dashboard.
+
+## Changelog
+### 1.0
+
+- Initial release
+
+## Frequently Asked Questions
+
+1. **Dose this plugin supports WPML or PolyLang ?**
+   - Comming Soon.
+
+2. **Dose this plugin supports Yoast SEO or MathRank ?**
+   - Comming Soon.
+
+# Credit
+- [Abdoo Mayhob](https://abdoo.me)
---
+++ est-read-time.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ *
+ * Plugin Name: Estimated Reading Time
+ * Plugin URI: https://github.com/Abdoo-mayhob/Estimated-Reading-Time
+ * Description: Calculate and Show Estimated Reading Time in your posts in Both Arabic and English.
+ * Version: 1.0.0
+ * Author: Abdoo
+ * Author URI: https://abdoo.me
+ * License: GPL3
+ * Text Domain: ert
+ * Domain Path: /languages
+ *
+ * ===================================================================
+ * 
+ * Copyright 2024  Abdullatif Al-Mayhob, Abdoo abdoo.mayhob@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 3, as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * ===================================================================
+ * 
+ * TODO:
+ * - Admin Dashboard
+ * - Calculator
+ * - Metafield
+ * - Shortcode
+ * - WPML Support
+ * - Polylang Support
+ * - Yoast SEO Support
+ * - Math Rank Support
+ * 
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Main Class.
+ */
+class EstReadTime {
+
+    function __construct()
+    {
+        
+    }
+
+}
